a4d9343076656d3008a13a76b9fe26b6f257d982
Langerais <dorfinmax@gmail.com>
2023-12-01 07:28:13 +0200

    Added Database connection using AWS secrets / DB connection test added and works now

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from getSecret import get_secret
+from sqlalchemy import text
+
 
 app = Flask(__name__)
 
-# Replace the below database URI with your actual database credentials
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+secret = get_secret()
+db_username = secret['username']
+db_password = secret['password']
+#db_name = secret['dbInstanceIdentifier']
+db_name = 'postgres'
+db_host = secret['host']
+db_port = secret['port']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
+
 
 db = SQLAlchemy(app)
 
 @app.route('/')
 def test_db():
     try:
-        db.engine.execute('SELECT 1')  # Simple query to test the database connection
-        return 'Database connection successful.'
+        with db.session.begin():
+            # Wrap the SQL query with text()
+            db.session.execute(text('SELECT 1'))
+            return 'Database connection successful.'
     except Exception as e:
         return f'An error occurred: {e}'
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    #print(test_db())
+    app.run(host='0.0.0.0', debug=True)
+
 
---
+++ getSecret.py
@@ -0,0 +1,33 @@
+# secrets_manager.py
+
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+
+def get_secret():
+    secret_name = "dev/capstone/admin"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    #print(secret)  # For debugging purposes only; remove after debugging
+
+    return json.loads(secret)  # Convert JSON string to Python dictionary
